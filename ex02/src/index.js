fe1c572571e127183f4b5250dcfb634bc0b463c8
AlmaIS2022 <almaisicspahovic@yahoo.com>
2022-04-12 08:49:01 +0200

    Initial commit

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function checkVariableScope() {
+    'use strict';
+
+    let i='function variable';
+    if (true) { 
+            i='block variable';
+return i;
+        console.log('Scope i is: ', i);
+    
+    }
+        console.log('Scope i is: ', i);
+        return i;
+}
+checkVariableScope();
+module.exports = checkVariableScope;
---
+++ ex04/src/index.js
@@ -0,0 +1,7 @@
+function myArr(mili) {
+    "use strict";
+    // Only change code below this line
+    const milili=[10, 25, 4];
+
+    printmyArr();
+    module.exports = myArr;
